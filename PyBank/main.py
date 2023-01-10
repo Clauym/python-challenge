52db1638a4a4bfb0ba0ba3f149cbb5754e63e920
Clauym <claudia.yurrita@gmail.com>
2023-01-10 12:30:25 -0600

    Bank analysis final version

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,106 +1,99 @@
 #BUDGET ANALYSIS FOR BANK
 # Initializing variables
-Tot_month=int(0)
-Sum_PL=0
-Change=float(0)
+tot_month=int(0)
+sum_pl=0
+change=float(0)
 cont=int(0)
 
-Avg_chg=float(0)
-Grt_inc=[]
-Grt_dec=[]
+avg_chg=float(0)
+grt_inc=[]
+grt_dec=[]
 Dates=[]
-PL_chg=[0]
-Row=[]
+pl_chg=[0]
+row_b=[]
 
 import csv
 import os 
 
 #Defining source file path    
-dir1="c:\\"
-dir2="Users"
-dir3="Celvir"
-dir4="python-challenge"
-dir5="PyBank"
-dir6="Resources"
-file_name="budget_data.csv"
 
-Main_file=os.path.join(dir1,dir2,dir3,dir4,dir5,dir6,file_name)
-print (Main_file)
-#os.path.normpath(Main_file)
+dir0='.'
+dir1='Resources'
+file_name='budget_data.csv'
 
-#print(Main_file)
-#Open the file to read. Did't specify if it ws just readable 
-#    due to the command messing with the route
+#Create the route to the input file
+main_file=os.path.join(dir0,dir1,file_name)
 
-with open (Main_file) as Src_File:
+#Open the file to read. 
+with open (main_file,"r") as src_file:
 
     #Check for the variables in the source    
-    Tot_line= csv.reader(Src_File, delimiter= ",")
+    tot_line=csv.reader(src_file, delimiter= ",")
     #Drop out the header
-    header = next(Tot_line)
+    header = next(tot_line)
     
     #Calculate the sum of montly PL 
-    for lines in Tot_line:
-        Tot_month+=1
-        Sum_PL=int(lines[1])+Sum_PL
+    for lines in tot_line:
+        tot_month+=1
+        sum_pl=int(lines[1])+sum_pl
 
         #Create a list for the dates that will serve as reference 
         Dates.append(lines[0])
        
        #trick to avoid a mistake in the first monthly difference
         if cont==0:
-            PL_past=int(lines[1])
+            pl_past=int(lines[1])
             cont+=1
         else:
-            PL_new=int(lines[1])
+            pl_new=int(lines[1])
             #Calculate the monthly change of PL
-            Change=PL_new - PL_past
-            PL_past=PL_new
+            change=pl_new - pl_past
+            pl_past=pl_new
             #Fill the list with PL values
-            PL_chg.append(Change)
+            pl_chg.append(change)
 
     #Find max and min of the monthly Pl changes
-    Grt_inc=max(PL_chg)
-    Grt_dec=min(PL_chg)
+    grt_inc=max(pl_chg)
+    grt_dec=min(pl_chg)
     
     #Look for the corresponding dates for the max and min in the Date string
-    F_maxi=Dates[PL_chg.index(Grt_inc)]
-    F_maxd=Dates[PL_chg.index(Grt_dec)]
+    F_maxi=Dates[pl_chg.index(grt_inc)]
+    F_maxd=Dates[pl_chg.index(grt_dec)]
     
     #Calculate the Avergae PL change
-    Avg_chg=sum(PL_chg)/ (Tot_month-1)
+    avg_chg=sum(pl_chg)/ (tot_month-1)
     
     #Calculate the date for date stamp
     from datetime import date 
     now = date.today( )
 
     #Set the data to be printed
-    Row.append("Financial Analysis")
-    Row.append("--------------")
-    Row.append(f"Total Months: {Tot_month}")
-    Row.append(f"Total: ${Sum_PL}")
-    Row.append(f"Average Change: ${round(Avg_chg,2)}")
-    Row.append(f"Greatest Increase in Profits: {F_maxi}  (${Grt_inc})")
-    Row.append(f"Greatest Decrease in Profits: {F_maxd} (${Grt_dec})")
-    Row.append('---')
-    Row.append(f"Data calculated as of {now}")
+    row_b.append("Financial Analysis")
+    row_b.append("--------------")
+    row_b.append(f"Total Months: {tot_month}")
+    row_b.append(f"Total: ${sum_pl}")
+    row_b.append(f"Average change: ${round(avg_chg,2)}")
+    row_b.append(f"Greatest Increase in Profits: {F_maxi}  (${grt_inc})")
+    row_b.append(f"Greatest Decrease in Profits: {F_maxd} (${grt_dec})")
+    row_b.append('---')
+    row_b.append(f"Data calculated as of {now}")
 
     #Display the analysis output at the terminal
-    for element in Row:
+    for element in row_b:
         print(element)
     
 #Name a file for the output data
-dir6="Analysis"
+dir1="Analysis"
 file_name="OutputBank.txt"
     
 
 #Create and open a txt file for the results 
-New_file=os.path.join(dir1,dir2,dir3,dir4,dir5,dir6,file_name)
-with open (New_file,"w" ) as Out_file: #Res_file:
+new_file=os.path.join(dir0,dir1,file_name)
+with open (new_file,"w" ) as out_file: #Res_file:
     
     #write analyisis results on the txt file
-    for element in Row:
+    for element in row_b:
         wr_line=element.split(",")
-        Out_file.write(element)
-        Out_file.write("\n")
+        out_file.write(element)
+        out_file.write("\n")
  
